3b1d8fb08f59976082583b97ee126a011bc64b48
Keren-Silagit config --global user.email <kerensilvanunes@gmail.com>
2026-09-05 11:59:55 -0300

    Atualiza rotas e dados dos candidatos, corrige links e formatação no README e nas views

--- README.md
+++ README.md
@@ -5,3 +5,5 @@ Repositório para o curso de node Senac-SMP 2026-2 modelo de projeto para aprend
 Set-ExecutionPolicy -ExecutionPolicy Bypass -Scope Process
 
 ```
+http://10.136.43.122:8080
+https://rickandmortyapi.com/api/character/avatar/1.jpeg
--- src/routes/candidatoRouter.js
+++ src/routes/candidatoRouter.js
@@ -1,5 +1,5 @@
-import { Router } from 'express'
-import { candidatos } from '../dados/candidatos.js'
+import { Router } from "express";
+import { candidatos } from "../dados/candidatos.js";
 
 const candidatoRouter = Router();
 
@@ -7,17 +7,15 @@ candidatoRouter.get('/candidatos', (req, res) => {
   res.json(candidatos)
 })
 
-candidatoRouter.get('/candidatos/:id', (req, res) => {
-  const numero = parseInt(req.params.id)
-  const candidato = candidatos.find(cand => cand.numeroUrna === numero)
- 
-  if (candidato) {
-    res.status(200)
-    res.json(candidato)
-  } else {
-    res.status(404).json({ error: 'Candidato não encontrado' })
-  }
-
+candidatoRouter.get('/candidato/:id', (req, res) => {
+    const numero = parseInt(req.params.id)
+    const candidato = candidatos.find(cand => cand.numeroUrna === numero)
+    if (candidato) {
+        res.status(200)
+        res.json(candidato)
+    } else {
+        res.status(404).json({ error: 'Candidato não encontrado' })
+    }
 })
 
 export default candidatoRouter
