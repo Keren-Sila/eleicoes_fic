cfd89d40652659d4df5f4d94325b79267b65b9fd
Keren-Silagit config --global user.email <kerensilvanunes@gmail.com>
2026-09-05 11:39:41 -0300

    estrutura inicial do projeto do curso - eleições fic

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # eleicoes_fic
 Repositório para o curso de node Senac-SMP 2026-2 modelo de projeto para aprender Backend
+
+```
+Set-ExecutionPolicy -ExecutionPolicy Bypass -Scope Process
+
+```
---
+++ index.js
@@ -0,0 +1,15 @@
+import express from 'express'
+import candidatoRouter from './src/routes/candidatoRouter.js'
+import candidatoViewRouter from './src/routes/usuariosRouter.js'
+import path from 'node:path'
+
+const app = express()
+app.set('view engine', 'ejs')
+app.set('views', path.join(path.resolve(), 'src', 'views'))
+app.use(express.json())
+app.use('/api', candidatoRouter)
+app.use('/', candidatoViewRouter)
+
+app.listen(3000, () => {
+  console.log('Server is running on http://localhost:3000')
+})
